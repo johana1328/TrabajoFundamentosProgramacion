b837a01d6beef4cdccfb7bdd3b73df1fdaa531a3
Mario Alejandro Mendez <marioalejandro.mendez.contractor@bbva.com>
2024-03-24 18:22:28 -0500

    ajustes

--- src/main/java/edu/poli/job/GenerateInfoFiles.java
+++ src/main/java/edu/poli/job/GenerateInfoFiles.java
@@ -1,9 +1,17 @@
 package edu.poli.job;
 
+import edu.poli.job.core.CustomException;
+import edu.poli.job.core.FileStorage;
+
 public class GenerateInfoFiles {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			FileStorage.buildRootDir();
+		} catch (CustomException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
--- src/main/java/edu/poli/job/core/FileStorage.java
+++ src/main/java/edu/poli/job/core/FileStorage.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class FileStorage {
 	private static final String FILE_CONFIG = "config.properties";
-	private static final String DIR_PATH = "";
+	private static final String DIR_PATH = "edu.poli.job.path.repository";
 
 	public static String getProperty(String property) throws CustomException {
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -16,22 +16,22 @@ public class FileStorage {
 		} catch (Exception e) {
 			throw new CustomException("Error al leer el archivos de propiedades");
 		}
-		if (props.contains(property)) {
+		if (props.containsKey(property)) {
 			return props.getProperty(property);
 		}
 		throw new CustomException("propiedad no encontrada");
 	}
-	
-	public static void buildRootDir() {
-		String dirPath = getProperty(FILE_CONFIG)
-		File dir = new File("/ruta/directorio_nuevo");
+
+	public static void buildRootDir() throws CustomException {
+		String dirPath = getProperty(DIR_PATH);
+		File dir = new File(dirPath);
 		if (!dir.exists()) {
-            if (dir.mkdirs()) {
-                System.out.println("Directorio creado");
-            } else {
-                System.out.println("Error al crear directorio");
-            }
-        }
+			if (dir.mkdirs()) {
+				System.out.println("Directorio creado");
+			} else {
+				System.out.println("Error al crear directorio");
+			}
+		}
 	}
 
 }
