9bc1426e4c12e39ff8735d72c8390e967eb0ebd6
Mario Alejandro Mendez <marioalejandro.mendez.contractor@bbva.com>
2024-03-24 18:05:58 -0500

    creacion de directorios

--- src/main/java/edu/poli/job/core/FileStorage.java
+++ src/main/java/edu/poli/job/core/FileStorage.java
@@ -1,10 +1,12 @@
 package edu.poli.job.core;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class FileStorage {
 	private static final String FILE_CONFIG = "config.properties";
+	private static final String DIR_PATH = "";
 
 	public static String getProperty(String property) throws CustomException {
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
@@ -18,6 +20,18 @@ public class FileStorage {
 			return props.getProperty(property);
 		}
 		throw new CustomException("propiedad no encontrada");
+	}
+	
+	public static void buildRootDir() {
+		String dirPath = getProperty(FILE_CONFIG)
+		File dir = new File("/ruta/directorio_nuevo");
+		if (!dir.exists()) {
+            if (dir.mkdirs()) {
+                System.out.println("Directorio creado");
+            } else {
+                System.out.println("Error al crear directorio");
+            }
+        }
 	}
 
 }
